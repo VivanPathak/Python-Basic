920e5036d8cc528a210cc17a5e2c35c946fb47b9
VivanPathak <92623408+VivanPathak@users.noreply.github.com>
2022-03-03 19:30:34 +0530

    Add files via upload

---
+++ PyCharm Files/Gui Intro.py
@@ -0,0 +1,15 @@
+import turtle
+
+My_Turtle = turtle.Turtle()
+
+My_Turtle.penup()
+My_Turtle.goto(50,75)
+My_Turtle.pendown()
+My_Turtle.forward(200)
+My_Turtle.left(90)
+My_Turtle.forward(200)
+My_Turtle.left(90)
+My_Turtle.forward(200)
+My_Turtle.left(90)
+My_Turtle.forward(200)
+turtle.done()
---
+++ PyCharm Files/Gui Intro_1.py
@@ -0,0 +1,13 @@
+import turtle
+
+My_Turtle = turtle.Turtle()
+
+My_Turtle.penup()
+My_Turtle.goto(50,75)
+My_Turtle.pendown()
+My_Turtle.forward(200)
+My_Turtle.left(120)
+My_Turtle.forward(200)
+My_Turtle.left(120)
+My_Turtle.forward(200)
+turtle.done()
